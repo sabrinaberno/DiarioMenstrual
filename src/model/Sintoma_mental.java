68f39fe957a6248d8016c7735421a66dc11b6f86
Mylena Angélica <86479209+Mylena-angelica@users.noreply.github.com>
2022-08-03 21:36:29 -0300

    Update Sintoma_mental.java
    Add construtor, set, get

--- src/model/Sintoma_mental.java
+++ src/model/Sintoma_mental.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 package model;
 
 
-public class Sintoma_mental {
+public class Sintoma_mental extends Sintoma {
 
 	private Enum nome_sintoma_mental;
+	
+	private Scanner ler = new Scanner(System.in);// Para os metodos
+	
+	// Metodo Construtor de Usuario
+	public Sintoma_mental(enum nome_sintoma_mental) {
+		super(data_do_cadastro, intensidade);
+		this.setData_do_cadastro(data_do_cadastro);
+		this.setIntensidade(intensidade);
+	}
+	// Getters e Setters
+	public enum getNome_sintoma_mental() {
+		return nome_sintoma_mental;
+	}
+	public void setNome_sintoma_mental(Enum []) {
+		
+	
 
 }
