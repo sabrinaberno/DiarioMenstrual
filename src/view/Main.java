23f1822fd8bdd7e43f4eddb88df4183bc6a356f3
Mylena Angélica <mylenangelica@gmail.com>
2022-08-11 16:28:03 -0300

    Create Main.java

---
+++ src/view/Main.java
@@ -0,0 +1,15 @@
+package view;
+
+/**
+ *
+ * @author mylen
+ */
+public class Main {   
+
+    public static void main(String[] args) {
+       new BancoDados();
+
+       JFrame TelaInicial = new TelaInicial();
+       telainicial.setVisible(true);
+    }
+}
