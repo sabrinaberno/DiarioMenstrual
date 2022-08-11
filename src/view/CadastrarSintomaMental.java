c1b40b042fb61a60692f303c3518926a18fe2f63
sabrinaberno <sabrinaberno@hotmail.com>
2022-08-11 17:26:04 -0300

    Finalizando tela de cadastro de sintoma mental

--- src/view/CadastrarSintomaMental.java
+++ src/view/CadastrarSintomaMental.java
@@ -25,6 +25,7 @@ import javax.swing.JFormattedTextField;
 import javax.swing.JComboBox;
 
 import model.Sintoma.Intensidade;
+import model.Sintoma_mental.Nome_sintoma_mental;
 
 
 public class CadastrarSintomaMental extends JFrame implements ActionListener {
@@ -33,14 +34,17 @@ public class CadastrarSintomaMental extends JFrame implements ActionListener {
 	private JPanel painel = new JPanel();
 	private static JFrame janela = new JFrame("Sintoma Mental");
 	private static JLabel titulo = new JLabel("Cadastro de Sintoma Mental");
-	private JComboBox <Intensidade> sintoma = new JComboBox<>();
-	private JButton cadastrar = new JButton("Cadastrar Ciclo");
-	private JLabel dataInicial = new JLabel("Data do início do ciclo :");
+	private JComboBox <Intensidade> intensidade = new JComboBox<>();
+	private JComboBox <Nome_sintoma_mental> nomeSintoma = new JComboBox<>();
+	private JButton cadastrar = new JButton("Cadastrar Sintoma");
+	private JLabel dataTxt = new JLabel("Data do sintoma mental (Ex: 15/02/2022): ");
 	//private JFormattedTextField dataInicialInput = new JFormattedTextField(new MaskFormatter("##/##/####"));
-	private JLabel dataFinal = new JLabel("Data do fim da menstruação :");
-	//private JFormattedTextField dataFinalInput = new JFormattedTextField(new MaskFormatter("##/##/####"));
-	
-	public CadastrarSintomaMental () {
+	JTextField data = new JTextField();
+	private JLabel nomeTxt = new JLabel("Selecione o seu sintoma: ");
+	private JLabel intensidadeTxt = new JLabel("Selecione a intensidade do seu sintoma: ");
+
+	public CadastrarSintomaMental ()
+	{
 		
 		janela.setBounds(100, 100, 600, 600);
 		painel.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -48,35 +52,55 @@ public class CadastrarSintomaMental extends JFrame implements ActionListener {
 		setContentPane(painel);
 		painel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		painel.setLayout(new BorderLayout(0, 0));
+				
 
-		titulo.setBounds(50, 0, 500, 50);
-		sintoma.setBounds(50, 100, 400, 100);
-		
-		titulo.setFont(new Font("Arial", Font.BOLD, 20));
+		titulo.setFont(new Font("Arial", Font.BOLD, 18));
 		titulo.setHorizontalAlignment(SwingConstants.CENTER);
+		titulo.setBounds(50, 0, 500, 50);
+		janela.add(titulo);
 		
-		sintoma.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-		sintoma.setModel(new DefaultComboBoxModel<>(Intensidade.values()));
+		
+		dataTxt.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		dataTxt.setBounds(50, 80, 400, 20);
+		data.setBounds(50, 120, 500, 30);
+		data.setColumns(10);
+		janela.add(data);
+		janela.add(dataTxt);
+		
+		
+		nomeTxt.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		nomeTxt.setBounds(50, 180, 400, 20);		
+		nomeSintoma.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		nomeSintoma.setModel(new DefaultComboBoxModel<>(Nome_sintoma_mental.values()));
+		nomeSintoma.setBounds(50, 210, 500, 30);
+		janela.add(nomeTxt);
+		janela.add(nomeSintoma); 
+		
+		
+		intensidadeTxt.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		intensidadeTxt.setBounds(50, 280, 400, 20);
+		intensidade.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		intensidade.setModel(new DefaultComboBoxModel<>(Intensidade.values()));
+		intensidade.setBounds(50, 310, 500, 30);
+		janela.add(intensidadeTxt);
+		janela.add(intensidade);
+		
+		
+		
 		
 		cadastrar.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-		cadastrar.setBounds(50, 450, 200, 50);
-
-		dataInicial.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		cadastrar.setBounds(50, 480, 500, 50);
+		janela.add(cadastrar);
 		
-		dataInicial.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-
+		
+		
+		
+		
+		
 		
 		janela.setLayout(null);
 		janela.setVisible(true);
 		
-		
-		janela.add(titulo);
-		janela.add(sintoma);
-		janela.add(cadastrar);
-		janela.add(dataInicial);
-		//janela.add(dataInicialInput);
-		janela.add(dataFinal);
-		//janela.add(dataFinalInput);
 		
 	}
 	
