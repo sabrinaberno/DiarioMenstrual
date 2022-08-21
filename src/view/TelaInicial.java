0cc384cb5b0a54ebb060901b6751ec691c0be3bc
Mylena Angélica <mylenangelica@gmail.com>
2022-08-21 19:06:18 -0300

    Update TelaInicial.java

--- src/view/TelaInicial.java
+++ src/view/TelaInicial.java
@@ -1,35 +1,49 @@
 package view;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JComponent;
 
-import model.Sintoma_fisico.Local_corpo;
-import model.Sintoma_fisico.Nome_sintoma_fisico;
-
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
 import java.awt.event.ActionListener;
-import java.text.ParseException;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
 
 
 public class TelaInicial extends JFrame implements ActionListener {
-
+	
 		private static JFrame janela = new JFrame("Meu Diário Menstrual");
-		private static JLabel titulo = new JLabel("Escolha qual ação você deseja executar");
+		private static JLabel titulo = new JLabel("O que você deseja fazer?");
 		private static JButton cadastro_usuario = new JButton("Cadastrar Pessoa");
 		private static JButton cadastro_ciclo = new JButton("Cadastrar novo Ciclo Menstrual");
 		private static JButton cadastro_sintoma_fisico = new JButton("Cadastrar um Sintoma Físico");
 		private static JButton cadastro_sintoma_mental = new JButton("Cadastrar um Sintoma Mental");
-		private static JButton relatorio = new JButton("Relatórios");
+		private static JButton relatorio = new JButton("Diários");
+		private JPanel painel = new JPanel (); 
+		private JLabel fundo = new JLabel ("");
 
 	//	public static ControleDados dados = new ControleDados();
 	 
-		private JPanel pane;
+		private static final long serialVersionUID = 1L;
 
 		
-		public TelaInicial() {
+		public TelaInicial() {	
+			
+			
+			fundo.setBounds(680, 382, 100, 69);
+			fundo.setIcon(new ImageIcon(TelaInicial.class.getResource("/view/imagens/back.png")));
+			add(fundo);
+			
+			setBackground(new Color(245, 245, 220));
+			setBounds(100, 100, 830, 522);
+			setLayout(null);
+			
 				
 			janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			//janela.setSize(400, 250);
@@ -38,12 +52,11 @@ public class TelaInicial extends JFrame implements ActionListener {
 			titulo.setBounds(50, 0, 500, 50);
 
 			//janela.setContentPane(pane);	
-			pane = new JPanel (); 
-			pane.setBorder(new EmptyBorder(5, 5, 5, 5));
-			pane.setLayout(new BorderLayout(0, 0));
+			painel.setBorder(new EmptyBorder(5, 5, 5, 5));
+			painel.setLayout(new BorderLayout(0, 0));
 
 			
-			titulo.setFont(new Font("Arial", Font.BOLD, 20));
+			titulo.setFont(new Font("Arial", Font.BOLD, 22));
 			titulo.setHorizontalAlignment(SwingConstants.CENTER);
 			
 			
@@ -60,18 +73,28 @@ public class TelaInicial extends JFrame implements ActionListener {
 			
 			
 			cadastro_usuario.setFont(new Font("Arial", Font.PLAIN, 20));
+			cadastro_usuario.setBackground(Color.orange);
+			cadastro_usuario.setForeground(Color.BLACK);
 			cadastro_usuario.setBounds(50, 70, 500, 50);
 			
 			cadastro_ciclo.setFont(new Font("Arial", Font.PLAIN, 20));
+			cadastro_ciclo.setBackground(Color.yellow);
+			cadastro_ciclo.setForeground(Color.BLACK);
 			cadastro_ciclo.setBounds(50, 150, 500, 50);
 			
 			cadastro_sintoma_fisico.setFont(new Font("Arial", Font.PLAIN, 20));
+			cadastro_sintoma_fisico.setBackground(Color.green);
+			cadastro_sintoma_fisico.setForeground(Color.BLACK);
 			cadastro_sintoma_fisico.setBounds(50, 230, 500, 50);
 			
 			cadastro_sintoma_mental.setFont(new Font("Arial", Font.PLAIN, 20));
+			cadastro_sintoma_mental.setBackground(Color.blue);
+			cadastro_sintoma_mental.setForeground(Color.white);			
 			cadastro_sintoma_mental.setBounds(50, 310, 500, 50);
 			
 			relatorio.setFont(new Font("Arial", Font.PLAIN, 20));
+			relatorio.setBackground(Color.red);
+			relatorio.setForeground(Color.white);			
 			relatorio.setBounds(50, 390, 500, 50);
 			
 			
@@ -80,6 +103,8 @@ public class TelaInicial extends JFrame implements ActionListener {
 		
 		public static void main(String[] args) {
 			TelaInicial menu = new TelaInicial();
+			
+			
 			
 						
 			cadastro_usuario.addActionListener(menu);
@@ -91,21 +116,36 @@ public class TelaInicial extends JFrame implements ActionListener {
 
 		public void actionPerformed(ActionEvent e) {
 			Object click = e.getSource();
-			
-			if(click == cadastro_usuario)
+
+			if(click == cadastro_usuario) {
 				new CadastrarPessoa();
+				janela.setVisible(false);
+			}
+				
 			
-			if(click == cadastro_ciclo)
+			if(click == cadastro_ciclo) {
 				new CadastrarCiclo();
+				janela.setVisible(false);
+			}
+				
 			
-			if(click == cadastro_sintoma_fisico)
+			if(click == cadastro_sintoma_fisico) {
 				new CadastrarSintomaFisico();
+				janela.setVisible(false);
+			}
+				
 			
-			if(click == cadastro_sintoma_mental)
+			if(click == cadastro_sintoma_mental) {
 				new CadastrarSintomaMental();
+				janela.setVisible(false);
+			}
+				
 			
-			if(click == relatorio)
-				new TelaRelatorio();
+			if(click == relatorio) {
+				new TelaDiario();
+				janela.setVisible(false);
+			}
+				
 		
 		}
 		
@@ -114,52 +154,3 @@ public class TelaInicial extends JFrame implements ActionListener {
 						
 			
 	}
-	/*
-	
-	// listando os locais do corpo
-			System.out.println("Locais do corpo:");
-			for(Local_corpo locais: Local_corpo.values()){
-				System.out.println(locais);
-			}
-		
-			System.out.println("Digite o local do corpo para cadastrar um sintoma fisico:");
-			Scanner corpo = new Scanner(System.in);
-
-			//listando os nomes das dores fisicas
-			System.out.println("\n Nome bla bla bla");
-			for(Nome_sintoma_fisico nome_fisico : Nome_sintoma_fisico.values()){
-				System.out.println(nome_fisico);
-			}
-	private final HomeController controller;
-
-	public Home() {
-		super("My Home Page");
-		this.controller = new HomeController(this);
-
-		this.setSize(900, 700);
-		this.setLayout(new GridLayout(3, 2));
-
-		this.labelLogin = new JLabel("Login:");
-		this.add(this.labelLogin);
-
-		this.fieldLogin = new JTextField();
-		this.add(this.fieldLogin);
-		
-		this.labelSenha = new JLabel("Senha:");
-		this.add(this.labelSenha);
-
-		this.fieldSenha = new JTextField();
-		this.add(this.fieldSenha);
-
-		this.cadastrar = new JButton("Cadastrar");
-		this.cadastrar.addActionListener(this);
-		this.add(this.cadastrar);
-
-		this.cancelar = new JButton("Cancelar");
-		this.cancelar.addActionListener(this);
-		this.add(this.cancelar);
-	}
-
-	*/
-
-
