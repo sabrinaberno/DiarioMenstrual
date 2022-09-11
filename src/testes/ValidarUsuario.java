e825d8bc858df66b882bbe94901e2a081f340cc7
sabrinaberno <sabrinaberno@hotmail.com>
2022-09-11 14:49:28 -0300

    Criando testes para validar usuário

--- src/testes/ValidarUsuario.java
+++ src/testes/ValidarUsuario.java
@@ -1,5 +1,34 @@
 package testes;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import controller.*;
+
+
 public class ValidarUsuario {
-    
+    ControlePessoa coUser = new ControlePessoa();
+	
+	@Test
+	void testValidarNome() {
+		assertTrue(coUser.validarNome("JUNIA"));
+		assertFalse(coUser.validarNome("Junia"));
+		assertFalse(coUser.validarNome("Juni4"));
+	}
+	
+	@Test
+	void testValidarEmail() {
+		assertFalse(coUser.validarEmail("JUNIA"));
+		assertTrue(coUser.validarEmail("JUNIA@"));
+		assertTrue(coUser.validarEmail("junia@"));
+		assertFalse(coUser.validarEmail("junia"));
+	}
+		
+	@Test
+	void testValidarSenha() {
+		assertFalse(coUser.validarSenha("JUN123456"));
+		assertFalse(coUser.validarSenha("1234567"));
+		assertFalse(coUser.validarSenha("jun1234"));
+		assertTrue(coUser.validarSenha("JUN1234"));
+	}
 }
